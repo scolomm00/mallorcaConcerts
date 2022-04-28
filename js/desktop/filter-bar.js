aecb5c9bc0706cc51ad9daa24ecc8ec773715c6a
scolomm00 <sebastiacolomoll@gmail.com>
2022-04-28 14:06:21 +0200

    mallorcaConcerts0428

---
+++ js/desktop/filter-bar.js
@@ -0,0 +1,8 @@
+let filterBar = document.getElementById("filter-bar");
+let filterButton = document.getElementById("filter-button");
+
+filterButton.addEventListener("click", () => {
+
+    filterBar.classList.toggle("active");
+
+});

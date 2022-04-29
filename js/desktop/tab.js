a34394be439658a1a2437765050e329d42665b67
scolomm00 <sebastiacolomoll@gmail.com>
2022-04-29 14:54:08 +0200

    mallorcaConcerts0429

---
+++ js/desktop/tab.js
@@ -0,0 +1,15 @@
+let tabs = document.querySelectorAll('.tab');
+
+tabs.forEach(tab => {
+
+    tab.addEventListener('click', () => {
+
+        tabs.forEach(tab => {
+
+            tab.classList.remove('active');
+        });
+
+        tab.classList.add('active');
+    });
+        
+});

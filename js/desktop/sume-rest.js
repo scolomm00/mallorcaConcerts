9aa379ce51501781b0cf7066e84a83ca038d1119
scolomm00 <sebastiacolomoll@gmail.com>
2022-05-03 09:47:27 +0200

    mallorcaConcerts0503

--- js/desktop/sume-rest.js
+++ js/desktop/sume-rest.js
@@ -2,19 +2,22 @@ export let sumeRest = () => {
 
     let restas = document.querySelectorAll(".rest");
     let sumas = document.querySelectorAll(".sume");
+    let counters = plus-minus.closest('.counter');
 
     sumas.forEach(suma => {
+
         suma.addEventListener("click", () => {
 
-            tickets.value = (parseInt(tickets.value) + 1);
+            counters.value = (parseInt(counters.value) + 1);
         });
-    })
+    });
 
     restas.forEach(resta => {
+
         resta.addEventListener("click", () => {
-    
+
             if (tickets.value > 1) {
-    
+
                 tickets.value = (parseInt(tickets.value) - 1);
             }
         });
